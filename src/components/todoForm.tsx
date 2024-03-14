c92da48b6a51ad2a40cefdedf69d4cd0feffa971
C1oudys <hymin310@gmail.com>
2024-03-14 16:32:06 +0900

    feat: form 페이지 새로고침 방지

--- src/components/todoForm.tsx
+++ src/components/todoForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { FormEvent } from 'react';
 
 export default function TodoForm() {
   const [title, setTitle] = useState('');
@@ -24,7 +25,8 @@ export default function TodoForm() {
     },
   });
 
-  const handleSubmit = async () => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
     if (!title || !content) {
       alert(`제목과 내용을 모두 입력해주세요.`);
       return;
@@ -39,17 +41,19 @@ export default function TodoForm() {
   return (
     <form className="flex flex-col space-y-4 items-center mt-5" onSubmit={handleSubmit}>
       <input
-        className="w-2/4 p-2 border rounded shadow-sm focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+        className="w-2/4 p-2 border text-black rounded shadow-sm focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
         placeholder="Title"
         type="text"
         value={title}
+        maxLength={30}
         onChange={(e) => setTitle(e.target.value)}
       />
       <input
-        className="w-2/4 p-2 border rounded shadow-sm focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
+        className="w-2/4 p-2 border text-black rounded shadow-sm focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500"
         placeholder="Content"
         type="text"
         value={content}
+        maxLength={40}
         onChange={(e) => setContent(e.target.value)}
       />
       <button
