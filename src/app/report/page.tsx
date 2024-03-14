e7e1c7e829b3115f613af8f6a4ed7e2fb6851fac
C1oudys <hymin310@gmail.com>
2024-03-14 17:29:45 +0900

    feat: repost 페이지 페칭방법 변경

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Todos } from '../types';
 
 async function ReportPage() {
-  const response = await fetch(`http://localhost:3000/api/todos`, { next: { revalidate: 10 } });
-  const { todosList }: { todosList: Todos[] } = await response.json();
-  const doingList = todosList.filter((item) => item.isDone === false);
-  const doneList = todosList.filter((item) => item.isDone === true);
+  const response = await fetch(`http://localhost:4000/todos`, { next: { revalidate: 10 } });
+  const todoList = await response.json();
+  const doingList = todoList.filter((item: Todos) => item.isDone === false);
+  const doneList = todoList.filter((item: Todos) => item.isDone === true);
 
   return (
     <main className="h-screen flex flex-col justify-start pt-20"> 
@@ -13,7 +13,7 @@ async function ReportPage() {
         <div className="flex flex-col items-center">
           <h1 className="text-2xl mb-3.5 text-center">할일 통계</h1>
           <p className="mt-3.5 leading-loose">
-            현재 <span className="font-bold text-red-500">{todosList.length}개</span>의 할일이
+            현재 <span className="font-bold text-red-500">{todoList.length}개</span>의 할일이
             있습니다.
             <br />
             해야할 할일은 <span className="font-bold text-red-500">{doingList.length}개</span>, 끝낸 할일은{' '}
