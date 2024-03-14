d3b8b69a17df0d6d51b188e15d04eb357801185d
C1oudys <hymin310@gmail.com>
2024-03-14 22:15:00 +0900

    design: ssr css 수정

--- src/components/reportLink.tsx
+++ src/components/reportLink.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const ReportLinkButton = () => {
   return (
-    <Link href="/report" className="mt-4 px-6 py-2 bg-green-500 text-white font-semibold rounded-lg shadow hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-700 focus:ring-opacity-50 transition ease-in-out duration-150">
+    <Link href="/report" className="mt-4 px-6 py-2 bg-black bg-opacity-30 text-white font-semibold rounded-lg shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 transition ease-in-out duration-150">
         할일 통계 보러가기
     </Link>
   );
