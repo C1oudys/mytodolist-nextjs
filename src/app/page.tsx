34731d671461e6eb893e8c60b39d60adfb29aa24
C1oudys <hymin310@gmail.com>
2024-03-14 16:15:39 +0900

    Design: 메인페이지 css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center">
-      <video autoPlay loop muted src="/assets/justdovideo.mp4" className="mt-8 w-full max-w-lg rounded-md shadow-lg" />
+    <main className="flex flex-col items-center justify-center mt-20">
+      <video autoPlay loop muted src="/assets/justdovideo.mp4" className="w-500 rounded-md shadow-lg" />
       <div className="mt-8 text-center">
         <h1 className="text-4xl text-white-500 font-bold">Just Do!!</h1>
         <h2 className="mt-4 text-2xl font-semibold text-gray-600">TodoList - Next.Js</h2>
