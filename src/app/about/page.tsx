f5316ed1cb474273b921ffb6885536ddbc700b03
C1oudys <hymin310@gmail.com>
2024-03-15 14:43:02 +0900

    fix: 코드수정

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,18 +3,19 @@ import { CompanyInfo } from '../types';
 
 async function AboutPage() {
   const response = await fetch(`http://localhost:4000/companyInfo`);
-  const { data }: { data: CompanyInfo } = await response.json();
-
+  const company: CompanyInfo = await response.json();
+  const { name, description, image } = company;
+  
   return (
     <main>
         <h1 className="text-2xl mb-6">회사 소개</h1>
         <div>
-          <img src={data.image} alt="회사 이미지" />
+          <img src={image} alt="회사 이미지" />
         </div>
         <div className="flex flex-col text-center">
-          <p >{data.name}</p>
+          <p >{name}</p>
           <p >
-            {data.description}
+            {description}
           </p>
         </div>
     </main>
